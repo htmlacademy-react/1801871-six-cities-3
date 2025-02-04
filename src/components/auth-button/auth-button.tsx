d1c05a396d0c8ac9d6b4f83b0df107895ca21ccc
viktor-study <victorbelenov11@gmail.com>
2025-02-04 08:26:05 +0300

    add link in auth button

--- src/components/auth-button/auth-button.tsx
+++ src/components/auth-button/auth-button.tsx
@@ -1,4 +1,6 @@
 import { AuthButtonStatus } from '../../const';
+import { Link } from 'react-router-dom';
+
 
 type ButtonStatus= {
   authButtonState: AuthButtonStatus;
@@ -7,9 +9,9 @@ type ButtonStatus= {
 function AuthButton ({authButtonState}:ButtonStatus):JSX.Element {
   return (
     <li className="header__nav-item">
-      <a className="header__nav-link" href="#">
+      <Link className="header__nav-link" to="/login">
         {authButtonState === AuthButtonStatus.LogIn ? <span className="header__signout">Sign out</span> : <span className="header__login">Sign in</span>}
-      </a>
+      </Link>
     </li>
   );
 }
