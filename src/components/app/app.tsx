cf53e8d086071823603739d841b02af690d24b29
viktor-study <victorbelenov11@gmail.com>
2025-02-04 19:14:22 +0300

    delete comment

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -19,29 +19,7 @@ type AppAmountOfPlaces = {
 
 function App({amountOfPlaces}:AppAmountOfPlaces): JSX.Element {
   return (
-  // <BrowserRouter>
-  //   <Routes>
-  //     <Route path={AppRoute.Root} element={<Layout authorizationStatus={AuthorizationsStatus.NoAuth}/>}>
-  //       <Route path={AppRoute.Root} element={<MainScreen amountOfPlaces={amountOfPlaces}/>}>
-  //       </Route>
 
-  //       <Route path={AppRoute.Offer} element={<OffersScreen/>}>
-  //       </Route>
-
-  //       <Route path={AppRoute.Favorite} element={
-  //         <PrivateRoute authorizationStatus={AuthorizationsStatus.Auth}>
-  //           <FavoritesScreen/>
-  //         </PrivateRoute>
-  //       }
-  //       />
-  //       <Route path={AppRoute.Login} element={<LoginScreen/>}>
-  //       </Route>
-
-  //       <Route path='*' element={<NotFoundScreen/>}>
-  //       </Route>
-  //     </Route>
-  //   </Routes>
-  // </BrowserRouter>
 
     <BrowserRouter>
       <Routes>
