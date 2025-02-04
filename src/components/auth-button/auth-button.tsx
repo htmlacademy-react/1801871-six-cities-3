a7daa77b3c95c9824b4b2281abd3fe8f29a92aee
viktor-study <victorbelenov11@gmail.com>
2025-02-04 08:42:25 +0300

    add approute in to

--- src/components/auth-button/auth-button.tsx
+++ src/components/auth-button/auth-button.tsx
@@ -1,5 +1,6 @@
 import { AuthButtonStatus } from '../../const';
 import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 
 type ButtonStatus= {
@@ -9,7 +10,7 @@ type ButtonStatus= {
 function AuthButton ({authButtonState}:ButtonStatus):JSX.Element {
   return (
     <li className="header__nav-item">
-      <Link className="header__nav-link" to="/login">
+      <Link className="header__nav-link" to={AppRoute.Login}>
         {authButtonState === AuthButtonStatus.LogOut ? <span className="header__signout">Sign out</span> : <span className="header__login">Sign in</span>}
       </Link>
     </li>
