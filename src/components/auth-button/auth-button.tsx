e70b4ac226a2d84d2da36a2c68f8eb7801fbb6e2
viktor-study <victorbelenov11@gmail.com>
2025-02-07 07:51:23 +0300

    add, object in auth button and bind status and classname and label

--- src/components/auth-button/auth-button.tsx
+++ src/components/auth-button/auth-button.tsx
@@ -7,11 +7,38 @@ type ButtonStatus= {
   authButtonState: AuthButtonStatus;
 }
 
+type AuthState = {
+  class:'header__signout';
+  label:'Sign out';
+} | {
+  class:'header__login';
+  label:'Sign in';
+} | {
+  class:'hide';
+  label:'';
+}
+
+const authState : Record<AuthButtonStatus, AuthState> = {
+  [AuthButtonStatus.LogIn]: {
+    class: 'header__signout',
+    label: 'Sign out'
+  },
+  [AuthButtonStatus.LogOut]: {
+    class: 'header__login',
+    label: 'Sign in'
+  },
+  [AuthButtonStatus.Hide]: {
+    class: 'hide',
+    label: ''
+  }
+};
+
 function AuthButton ({authButtonState}:ButtonStatus):JSX.Element {
+  const state = authState[authButtonState];
   return (
     <li className="header__nav-item">
       <Link className="header__nav-link" to={AppRoute.Login}>
-        {authButtonState === AuthButtonStatus.LogOut ? <span className="header__signout">Sign out</span> : <span className="header__login">Sign in</span>}
+        <span className={state.class}>{state.label}</span>
       </Link>
     </li>
   );
