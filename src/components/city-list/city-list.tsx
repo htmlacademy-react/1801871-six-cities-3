4410019d7bb56d62b0c00ae11d3251195b7e2d85
ViktorBelenov <82025167+ViktorBelenov@users.noreply.github.com>
2025-03-29 20:28:59 +0300

    add active city from state

--- src/components/city-list/city-list.tsx
+++ src/components/city-list/city-list.tsx
@@ -1,36 +1,48 @@
+import { City } from '../../types/offers';
 
-function CityList():JSX.Element {
+type CityListProps = {
+  city:City;
+}
+
+function CityList({city}: CityListProps):JSX.Element {
+
+  const markActiveCity = function(name:string, activeCityName:string):string {
+    if(activeCityName === name) {
+      return 'tabs__item--active';
+    }
+    return '';
+  };
 
   return (
     <section className="locations container">
       <ul className="locations__list tabs__list">
         <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="#">
+          <a className={`locations__item-link tabs__item ${markActiveCity('Paris', city.name)}`} href="#" >
             <span>Paris</span>
           </a>
         </li>
         <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="#">
+          <a className={`locations__item-link tabs__item ${markActiveCity('Cologne', city.name)}`} href="#">
             <span>Cologne</span>
           </a>
         </li>
         <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="#">
+          <a className={`locations__item-link tabs__item ${markActiveCity('Brussels', city.name)}`} href="#">
             <span>Brussels</span>
           </a>
         </li>
         <li className="locations__item">
-          <a className="locations__item-link tabs__item tabs__item--active">
+          <a className={`locations__item-link tabs__item ${markActiveCity('Amsterdam', city.name)}`} href="#">
             <span>Amsterdam</span>
           </a>
         </li>
         <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="#">
+          <a className={`locations__item-link tabs__item ${markActiveCity('Hamburg', city.name)}`} href="#">
             <span>Hamburg</span>
           </a>
         </li>
         <li className="locations__item">
-          <a className="locations__item-link tabs__item" href="#">
+          <a className={`locations__item-link tabs__item ${markActiveCity('Dusseldorf', city.name)}`} href="#">
             <span>Dusseldorf</span>
           </a>
         </li>
--- src/сities.ts
+++ src/сities.ts
@@ -1,7 +1,7 @@
 import { City } from './types/offers';
 
 export const CITY: City = {
-  name: 'Амстердам',
+  name: 'Amsterdam',
   location:{
     latitude: 52.3909553943508,
     'longitude': -4.85309666406198,
