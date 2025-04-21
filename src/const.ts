e8e0a2c0f555890e7edce496cc0225f3d1297e04
viktor-study <victorbelenov11@gmail.com>
2025-04-21 20:14:24 +0300

    fix

--- src/const.ts
+++ src/const.ts
@@ -24,9 +24,6 @@ export enum AuthLayoutState {
 export type NotFoundPageType = 'offer' | 'route';
 
 
-export const ValidID: Set<string | undefined> = new Set(['8b3693d9-e512-4377-bf3f-fba0d7c26984', '4da58b5e-1a67-40f0-b998-2af668bbdfc8', '9a374ac2-2af3-4c70-9f8d-a7ccbaa4de1c', '28c808ba-edd8-4934-b095-2cf8ca59905a']);
-
-
 export const URL_MARKER_DEFAULT =
   'img/pin/default-pin.svg';
 
