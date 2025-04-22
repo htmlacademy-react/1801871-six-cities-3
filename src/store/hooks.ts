834b22e877923e648b33c8562283ac4dac5b9aeb
viktor-study <victorbelenov11@gmail.com>
2025-04-22 20:42:02 +0300

    fix

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
-import type {State, AppDispatch} from '../types/state';
-=======
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { store } from './store';
 
@@ -9,7 +5,6 @@ type State = ReturnType<typeof store.getState>;
 
 type AppDispatch = typeof store.dispatch;
 
->>>>>>> master
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
