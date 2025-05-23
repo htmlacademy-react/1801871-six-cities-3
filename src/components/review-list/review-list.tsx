05af5a4962befe615441eb2dd0283403e8135158
ViktorBelenov <82025167+ViktorBelenov@users.noreply.github.com>
2025-05-23 21:38:53 +0300

    add in offer check for auth

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -9,6 +9,7 @@ import { sendComment } from '../../store/api-action';
 import ErrorText from '../error-text/error-text';
 
 import { ENDPOINTS } from '../../types/endpoint';
+import { AuthState } from '../../const';
 
 type ReviewListProps = {
   comments: TComment[];
@@ -23,6 +24,7 @@ function ReviewList ({comments , id}:ReviewListProps):JSX.Element {
 
   const dispatch = useAppDispatch();
   const errorData = useAppSelector((state)=>state.errorData);
+  const authStatus = useAppSelector((state)=> state.authStatus);
 
   const rating = [
     {title: 'perfect', value: 5},
@@ -80,60 +82,62 @@ function ReviewList ({comments , id}:ReviewListProps):JSX.Element {
         {comments.map((element) =>
           (<Comment comment={element} key={element.id} />))}
       </ul>
-      <form className="reviews__form form" action="#" method="post" onSubmit={onSubmitComment}>
-        <label className="reviews__label form__label" htmlFor="review">
+      { authStatus === AuthState.Auth ?
+        <form className="reviews__form form" action="#" method="post" onSubmit={onSubmitComment}>
+          <label className="reviews__label form__label" htmlFor="review">
     Your review
-        </label>
-        <div className="reviews__rating-form form__rating">
-          {rating.map((rate)=>(
-            <Fragment key={rate.title}>
-              <input
-                className="form__rating-input visually-hidden"
-                name="rating"
-                defaultValue={rate.value}
-                id={`${rate.value}-stars`}
-                type="radio"
-                checked={comment.rating === rate.value}
-                onChange={getCommentHandler('rating')}
-              />
-              <label
-                htmlFor={`${rate.value}-stars`}
-                className="reviews__rating-label form__rating-label"
-                title="perfect"
-              >
-                <svg className="form__star-image" width={37} height={33}>
-                  <use xlinkHref="#icon-star" />
-                </svg>
-              </label>
-            </Fragment>))}
+          </label>
+          <div className="reviews__rating-form form__rating">
+            {rating.map((rate)=>(
+              <Fragment key={rate.title}>
+                <input
+                  className="form__rating-input visually-hidden"
+                  name="rating"
+                  defaultValue={rate.value}
+                  id={`${rate.value}-stars`}
+                  type="radio"
+                  checked={comment.rating === rate.value}
+                  onChange={getCommentHandler('rating')}
+                />
+                <label
+                  htmlFor={`${rate.value}-stars`}
+                  className="reviews__rating-label form__rating-label"
+                  title="perfect"
+                >
+                  <svg className="form__star-image" width={37} height={33}>
+                    <use xlinkHref="#icon-star" />
+                  </svg>
+                </label>
+              </Fragment>))}
 
 
-        </div>
-        <textarea
-          className="reviews__textarea form__textarea"
-          id="review"
-          name="review"
-          placeholder="Tell how was your stay, what you like and what can be improved"
-          defaultValue={comment.comment}
-          onChange={getCommentHandler('comment')}
-        />
-        {showHideError()}
-        <div className="reviews__button-wrapper">
-          <p className="reviews__help">
+          </div>
+          <textarea
+            className="reviews__textarea form__textarea"
+            id="review"
+            name="review"
+            placeholder="Tell how was your stay, what you like and what can be improved"
+            defaultValue={comment.comment}
+            onChange={getCommentHandler('comment')}
+          />
+          {showHideError()}
+          <div className="reviews__button-wrapper">
+            <p className="reviews__help">
       To submit review please make sure to set{' '}
-            <span className="reviews__star">rating</span> and describe
+              <span className="reviews__star">rating</span> and describe
       your stay with at least{' '}
-            <b className="reviews__text-amount">50 characters</b>.
-          </p>
-          <button
-            className="reviews__submit form__submit button"
-            type="submit"
-            disabled={comment.comment.length <= 50}
-          >
+              <b className="reviews__text-amount">50 characters</b>.
+            </p>
+            <button
+              className="reviews__submit form__submit button"
+              type="submit"
+              disabled={comment.comment.length <= 50}
+            >
       Submit
-          </button>
-        </div>
-      </form>
+            </button>
+          </div>
+        </form> : ''}
+
     </section>
   );
 }
