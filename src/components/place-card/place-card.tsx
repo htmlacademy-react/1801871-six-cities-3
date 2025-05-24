3f0a06910774fb4b2ae66a0ef42a6cf3816176b1
ViktorBelenov <82025167+ViktorBelenov@users.noreply.github.com>
2025-05-24 10:29:30 +0300

    fix path

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -75,7 +75,7 @@ function PlaceCard({offer, handelCurrentActiveCard, type} : PlaceCardProps): JSX
           </div>
         </div>
         <h2 className="place-card__name">
-          <Link to={`offer/${offer.id}`}>
+          <Link to={`/offer/${offer.id}`}>
             {offer.title}
           </Link>
         </h2>
